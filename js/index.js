65889634c6374c666669f59c8e68a3298a23ea7e
sherlock <740785956@qq.com>
2019-05-11 22:09:26 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(function(){
+    $(window).on("resize",function(){
+        // 获取当前窗口宽度
+        let clientW = $(window).width()
+        
+        // 判断是否为小设备
+        let charge = clientW > 800
+        
+        // 获取所有的item
+        let $Iitems = $("#cg_banner .item")
+        
+        // 分别对大小设备进行处理
+        $Iitems.each(function(index,item){
+            let imgUrl = charge?$(item).data("sm-img"):$(item).data("lg-img")
+            $(item).css({      
+                backgroundImage:'url("'+ imgUrl +'")',
+            })
+            if(charge){
+                $(item).empty()
+              
+            }else{
+                let img = '<img src="'+ imgUrl +'" alt="" >'
+                $(item).empty().append(img)
+            }
+        })
+        
+        
+    })
+
+    $(window).trigger("resize")
+})
